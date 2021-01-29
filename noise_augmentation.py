e71a6b0c636f9c3ec1ef4dac563d9e7b5f5b64f8
birallex <biralex204@gmail.com>
2021-01-29 23:01:47 +0300

    add noise augmentation(.ipynb and .py)
    add augmentation in the form of noise
    add example image
    add result of noise function work

---
+++ noise_augmentation.py
@@ -0,0 +1,22 @@
+from keras.preprocessing import image
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+def noise(source_matrix, noise_level):
+    result = np.copy(source_matrix)
+    for x in range(source_matrix.shape[0]):
+        for y in range(source_matrix.shape[1]):
+            if result[x][y].any() != 0:
+                noise = random.randint(-noise_level, noise_level)
+                result[x][y] += noise
+    result = np.clip(result, 0, 255) 
+    return result
+
+
+if __name__ == "__main__":
+    img = image.load_img("input_image.jpg")
+    image_matrix = image.img_to_array(img)
+    output_img_matrix = noise(image_matrix, 100)
+    output_image = image.array_to_img(output_img_matrix)
+    output_image.save("noise_augmentation_output.jpg")
