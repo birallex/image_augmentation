e7cff3e81eece64c1ee1bb0e468cd802431cff78
birallex <biralex204@gmail.com>
2021-02-25 23:35:22 +0300

    fix tabs error

--- degradate_augmentation/degrade_augmentation.py
+++ degradate_augmentation/degrade_augmentation.py
@@ -3,13 +3,15 @@ import numpy as np
 import pandas as pd
 import cv2
 
+
 def prepare_images(img, factor):               
-        h, w, _ = img.shape
-        new_height = int(h / factor)
-        new_width = int(w / factor)
-        img = cv2.resize(img, (new_width, new_height), interpolation = cv2.INTER_LINEAR)
-        img = cv2.resize(img, (w, h), interpolation = cv2.INTER_LINEAR)
-        return img
+    h, w, _ = img.shape
+    new_height = int(h / factor)
+    new_width = int(w / factor)
+    img = cv2.resize(img, (new_width, new_height), interpolation = cv2.INTER_LINEAR)
+    img = cv2.resize(img, (w, h), interpolation = cv2.INTER_LINEAR)
+    return img
+
 
 if __name__ == "__main__":
     img = image.load_img("../input_image.jpg")
